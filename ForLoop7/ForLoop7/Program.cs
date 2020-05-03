d114f0b887dda3acd1313c5e07fa546b4da444b1
CARICOM12 <0190a@ukr.net>
2020-05-03 14:53:53 +0300

    Add files via upload

---
+++ ForLoop7/ForLoop7/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace ForLoop7
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            //for (int i = 0; i <= 10; i++)
+                //Console.WriteLine(i); 
+            for (int b = 100; b >= 10; b++)
+                Console.WriteLine(b); //бесконечный цикл
+        }
+    }
+}
